501b5333bb32b51b8b93120fbffafee1a324ad5b
mostafaIn <mostafa2015se@gmail.com>
2019-12-23 13:24:59 +0200

    FlexBox Basics

--- App.js
+++ App.js
@@ -50,19 +50,22 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#eee',
     paddingTop:40,
     paddingHorizontal:20,
     // alignItems: 'center',
     // justifyContent: 'center',
   },
   content:{
-    padding:40
+    flex:1,
+    backgroundColor: '#fff',
+    padding:40,
   },
   list:{
+    flex:1,
+    backgroundColor: '#ddd',
     marginTop: 24,
     padding:30,
-    backgroundColor: '#fff',
     fontSize: 24,
   }
 });
